6c970e8cd1fb72b1c9a8a780d44fb7036b0497e7
GenDev0 <dev.Gen.info0@gmail.com>
2025-07-16 16:31:22 +0100

    export all needed files

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { MultiSelect } from "./components/MultiSelect";
+export { DataMultiSelect } from "./components/DataMultiSelect";
+
+// Optionally export cn utility if you want to make it available
+export { cn } from "./utils/cn";
