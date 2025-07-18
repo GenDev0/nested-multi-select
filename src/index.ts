5407cfec0f3646f8e1224f55ad0cea264afdbc27
GenDev0 <dev.Gen.info0@gmail.com>
2025-07-18 14:55:33 +0100

    Update Index to export the new nestedMultiSelect

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 export { MultiSelect } from "./components/MultiSelect.js";
 export { DataMultiSelect } from "./components/DataMultiSelect.js";
+export { NestedMultiSelect } from "./components/NestedMultiSelect.js";
 
-// Optionally export cn utility if you want to make it available
 export { cn } from "./utils/cn.js";
+
+// Export types separately (for TypeScript users)
+export type {
+  NestedMultiSelectOption,
+  NestedMultiSelectValue,
+} from "./components/NestedMultiSelect.js";
